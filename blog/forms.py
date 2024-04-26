12035958eddde8c7b6b41745fb85ac8235fca5f6
yosef <yosfemyau@gmail.com>
2024-04-26 14:35:36 +0300

    update forms relative to Mptt modelform

--- blog/forms.py
+++ blog/forms.py
@@ -3,7 +3,11 @@ from .models import Comment
 from mptt.forms import TreeNodeChoiceField
 
 class CommentForm(forms.ModelForm):
-    parent = TreeNodeChoiceField(queryset=Comment.objects.all() )
+    parent = TreeNodeChoiceField(
+        queryset=Comment.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False
+    )  
     class Meta:
         model = Comment
         fields = ('name','parent', 'email', 'content')
