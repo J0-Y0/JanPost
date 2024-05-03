f3813a2b6594aa75f1c5cfdfcfdcf56a2bafb8ec
yosef <yosfemyau@gmail.com>
2024-05-04 01:23:03 +0300

    account reset featur

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('login/',views.user_login,name='login'),
-    path('signup/',views.user_signup,name='signup'),
     path('logout/',views.user_logout,name='logout'),
+
+    path('signup/',views.user_signup,name='signup'),
     path('activate/<str:uidb64>/<str:token>',views.activate_account,name='activate'),
+
+    path('reset_request/',views.password_reset_Request,name='reset_request'),
+    path('reset/<uidb64>/<token>',views.reset_account,
+        name='reset'),    
+    
 
     path('profile/',views.user_profile,name='profile'),
 ]
