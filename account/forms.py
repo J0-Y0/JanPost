663a426f613955e4f7b0ab9f1fc566be4671e66b
yosef <yosfemyayu@gmail.com>
2024-05-04 18:04:41 +0300

    password reset token and link invalidation fixed
    but password validation is still an issue

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm,SetPasswordForm
+from django.contrib.auth.forms import UserCreationForm,SetPasswordForm,PasswordResetForm
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=40, required=True,
@@ -37,24 +37,24 @@ class SignupForm(UserCreationForm):
                 raise forms.ValidationError("Email already taken")
             return email
 
-class PwdResetRequestForm(forms.Form):
+class PwdResetRequestForm(PasswordResetForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email'}))
     def clean_email(self):
         email = self.cleaned_data['email']
         if not User.objects.filter(email=email).exists():
             raise forms.ValidationError("Sorry, we could not find your account.")
         return email
-# class PwdResetForm(SetPasswordForm):
-#     new_password1 = forms.CharField(
-#         label='New password', widget=forms.PasswordInput(
-#             attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-newpass'}))
-#     new_password2 = forms.CharField(
-#         label='Repeat password', widget=forms.PasswordInput(
-#             attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
+class PwdResetForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label='New password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-newpass'}))
+    new_password2 = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
     
-#     def clean_new_password2(self):
-#             password1 = self.cleaned_data['password1']
-#             password2 = self.cleaned_data['password2']
-#             if password1 and password2 and password1!= password2:
-#                 raise forms.ValidationError("Passwords don't match")
-#             return new_password2
+    def clean_new_password2(self):
+            password1 = self.cleaned_data['new_password1']
+            password2 = self.cleaned_data['new_password2']
+            if password1 and password2 and password1!= password2:
+                raise forms.ValidationError("Passwords don't match")
+            return password2
--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-
+from .forms import PwdResetRequestForm,PwdResetForm
 
 urlpatterns = [
     path('login/',views.user_login,name='login'),
@@ -10,11 +10,12 @@ urlpatterns = [
     path('signup/',views.user_signup,name='signup'),
     path('activate/<str:uidb64>/<str:token>',views.activate_account,name='activate'),
 
-    path('reset_request/',views.password_reset_Request,name='reset_request'),
-    path('reset/<uidb64>/<token>',views.reset_account,
-        name='reset'),    
-    
-
+    #Password reset| using django default class view 
+    path('password_reset_request/',auth_views.PasswordResetView.as_view(form_class=PwdResetRequestForm),name='pwdreset_request'),
+    path('password_reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(form_class=PwdResetForm), name="pwdreset"),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/done',auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+              
     path('profile/',views.user_profile,name='profile'),
 ]
 
