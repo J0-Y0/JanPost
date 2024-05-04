9e4ca8842e80df5f6b194e25dbfc1045cbf91a88
yosef <yosfemyau@gmail.com>
2024-05-04 23:06:10 +0300

    update navbare
    add user avator for authenticated user and login btn for onthers

--- account/models.py
+++ account/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+def user_avatar_path(instance,filename):
+    return 'users/{0}/{1}'.format(instance.id,filename)
+
+    
+class Profile(models.Model):
+    user =models.OneToOneField(User,on_delete=models.CASCADE,)
+    bio = models.TextField(max_length = 500 ,blank = True,default="just am using")
+    avatar = models.ImageField(upload_to=user_avatar_path, blank = True,default="user/default.jpg")
+    
+    def __str__(self):
+        return self.user.username
+        
+@ receiver(post_save,sender=User)      
+def create_user_profile(sender,instance,created,**kwargs):
+    if created :
+        Profile.objects.create(user = instance)
+@receiver(post_save, sender=User) 
+def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
